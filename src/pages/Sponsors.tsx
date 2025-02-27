15f18f432edf92fa8b0250a57d8f29d6ec917f95
Szymon Gosk <ideal.szymongosk@gmail.com>
2025-02-27 21:40:48 +0100

    Final changes

--- src/pages/Sponsors.tsx
+++ src/pages/Sponsors.tsx
@@ -2,26 +2,31 @@
 const Sponsors = () => {
   const sponsors = [
     {
-      name: "Sponsor Złoty",
-      logo: "/logos/university.png",
+      name: "Fundacja L i D",
+      logo: "/sponsors/fundacja.jpg",
     },
     {
-      name: "Sponsor Srebrny",
-      logo: "/logos/university.png",
+      name: "Koło gospodyń wiejskich w Sokółce",
+      logo: "/sponsors/kolo-gospodyn.png",
     },
     {
-      name: "Sponsor Brązowy",
-      logo: "/logos/university.png",
+      name: "Barbara Okuła, posłanka na Sejm RP",
+      logo: "/sponsors/female.png",
     },
     {
-      name: "Sponsor Wspierający",
-      logo: "/logos/university.png",
+      name: "Urząd Miejski w Białymstoku",
+      logo: "/sponsors/sponsor-bialystok.png",
     },
     {
-      name: "Sponsor Technologiczny",
-      logo: "/logos/group.png", // This is the rectangular logo
+      name: "Urząd Marszałkowski Województwa Podlaskiego",
+      logo: "/sponsors/sponsor-wojewodztwo.png",
+    },
+    {
+      name: "Urząd Marszałk",
+      logo: "/sponsors/sponsor-wojewodztwo.png",
     },
   ];
+
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
@@ -30,7 +35,7 @@ const Sponsors = () => {
       {/* Sponsors */}
       <div className="bg-white rounded-lg shadow-sm p-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {sponsors.slice(0, 3).map((sponsor, index) => (
+          {sponsors.slice(0, 5).map((sponsor, index) => (
             <div
               key={index}
               className="flex flex-col items-center justify-center p-4"
@@ -40,23 +45,6 @@ const Sponsors = () => {
                 alt={sponsor.name}
                 className="h-24 w-auto mb-4"
               />
-              <p className="text-center text-gray-600">{sponsor.name}</p>
-            </div>
-          ))}
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8">
-          {sponsors.slice(3).map((sponsor, index) => (
-            <div
-              key={index + 3}
-              className="flex flex-col items-center justify-center p-4"
-            >
-              <div className="h-24 flex items-center justify-center">
-                <img
-                  src={sponsor.logo}
-                  alt={sponsor.name}
-                  className="h-24 w-auto mb-4"
-                />
-              </div>
               <p className="text-center text-gray-600">{sponsor.name}</p>
             </div>
           ))}
